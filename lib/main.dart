e3be0cb32df66d393cff7c954ce38899bc6e8f6c
Joelete López Ibañez <jlopiba1@upv.edu.es>
2025-10-15 17:26:46 +0200

    añadimos color al fondo, seguimos ampliando

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
       title: 'Práctica 1',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: MenuPrincipal(),
-      
     );
   }
 }
@@ -28,63 +27,61 @@ class MenuPrincipal extends StatelessWidget {
         title: Text('Ejercicios Clase Práctica 1'),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Mis ejercicios',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.blue,
-              ),
-            ),
-            SizedBox(height: 40),
-            Container(
-              color: Colors.amber,
-              width: 200,
-              height: 400,
-              child: const Text('Hello, Flutter!'),
-            ),
-
-            // Botón Ejercicio 1
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Ejercicio1()),
-                );
-              },
-              child: Container(
-                width: 250,
-                height: 60,
-                margin: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                  color: Colors.red,
-                  borderRadius: BorderRadius.circular(10),
+      body: Container(
+        // Aquí definimos el color de fondo verde claro
+        color: Colors.lightGreenAccent,
+        width: double.infinity,
+        height: double.infinity,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Mis ejercicios',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
                 ),
-                child: Center(
-                  child: Text(
-                    'Ejercicio 1: Tarjeta personal',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+              ),
+              SizedBox(height: 40),
+              Container(
+                color: Colors.amber,
+                width: 200,
+                height: 400,
+                child: const Center(child: Text('Hello, Flutter!')),
+              ),
+
+              // Botón Ejercicio 1
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Ejercicio1()),
+                  );
+                },
+                child: Container(
+                  width: 250,
+                  height: 60,
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Ejercicio 1: Tarjeta personal',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-
-          
-
-            
-
-          
-
-            
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -98,9 +95,10 @@ class Ejercicio1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Ejercicio 1'), backgroundColor: Colors.red),
-
-
+      appBar: AppBar(
+        title: Text('Ejercicio 1'),
+        backgroundColor: Colors.red,
+      ),
       body: Placeholder(),
     );
   }
