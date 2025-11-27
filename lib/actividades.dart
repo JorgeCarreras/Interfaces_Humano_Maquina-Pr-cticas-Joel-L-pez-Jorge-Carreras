1124010ff9aea1d7712a876499258a1f05f30f73
JorgeCarreras <jjcarcas@upv.edu.es>
2025-11-27 18:08:55 +0100

    Ampliamos la clase Item con más información para gastarla en la siguiente pantalla

--- lib/actividades.dart
+++ lib/actividades.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-// Clase modelo
+
 class Item {
   final int id;
   final String titulo;
   final String descripcion;
   final String imagePath;
 
+  final String precio;           // Ej: "30€/hora"
+  final String duracion;         // Ej: "1 hora"
+  final String nivel;            // Ej: "Todos los niveles"
+  final String descripcionLarga; // Texto extendido
+  final double rating;           // Ej: 4.5
+
   Item({
     required this.id,
     required this.titulo,
     required this.descripcion,
     required this.imagePath,
+    required this.precio,
+    required this.duracion,
+    required this.nivel,
+    required this.descripcionLarga,
+    required this.rating,
   });
 }
+
 
 // Pantalla de actividades
 class ActividadesPage extends StatelessWidget {
@@ -29,18 +41,36 @@ class ActividadesPage extends StatelessWidget {
         titulo: 'Surf',
         descripcion: 'Clases para todos los niveles.',
         imagePath: 'assets/img/surf.jpg',
+        precio: '20€/hora',
+        duracion: '1.30 hora',
+        nivel: 'Todos los niveles',
+        descripcionLarga:
+            'Disfruta del surf en las mejores condiciones. Nuestros instructores te ayudarán a progresar desde el primer día.',
+        rating: 4.8,
       ),
       Item(
         id: 2,
         titulo: 'Windsurf',
         descripcion: 'Aprovecha el viento y deslízate sobre las olas.',
         imagePath: 'assets/img/windsurf.jpeg',
+        precio: '25€/hora(equpo completo tabla+vela)',
+        duracion: '2 hora',
+        nivel: 'Iniciación y Perfeccionamineto',
+        descripcionLarga:
+            'Disfruta del Windsurf gracias al garbí , Iniciación por las mañanas perfccionamiento por las tardes.',
+        rating: 4.8,
       ),
       Item(
         id: 3,
         titulo: 'Paddle Surf',
         descripcion: 'Disfruta del mar con equilibrio y diversión.',
         imagePath: 'assets/img/paddelsurf.png',
+        precio: '15€/hora',
+        duracion: '1 hora',
+        nivel: 'Todos los niveles',
+        descripcionLarga:
+            'Disfruta relajandote con el mar en calma, consultar disponibilidad de tablas rigidas o hinchables ',
+        rating: 4.8,
       ),
       // NUEVA Clase WINGFOIL
       Item(
@@ -48,12 +78,24 @@ class ActividadesPage extends StatelessWidget {
         titulo: 'Wingfoil',
         descripcion: 'Descubre este nuevo deporte recien llegado de Estados Unidos',
         imagePath: 'assets/img/WingFoil.jpg',
+        precio: '30€/hora',
+        duracion: '1 hora',
+        nivel: 'Solo clases de iniciación ',
+        descripcionLarga:
+            'Aprende de la mano de nuestros monitores, a utilizar el foil de la manera mas eficiente ',
+        rating: 4.8,
       ),
       Item(
         id: 5,
         titulo: 'Kitesurf',
         descripcion: 'Vuela sobre el mar y disfruta de la adrenalina del viento.',
         imagePath: 'assets/img/kitesurf.jpg',
+        precio: '30€/hora',
+        duracion: '1 hora',
+        nivel: 'Solo iniciación ',
+        descripcionLarga:
+            'Clases solo con previsión de vientos por encima de 11 nudos',
+        rating: 4.8,
       ),
     ];
 
